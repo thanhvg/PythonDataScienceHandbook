b22b63d4e4c104739358ec7491dddc392c3957bb
Thanh Vuong <thanhvg@gmail.com>
2019-11-14 23:56:20 -0700

    89

--- src/hello.py
+++ src/hello.py
@@ -5,6 +5,9 @@ import seaborn
 
 plt = matplotlib.pyplot
 seaborn.set()
+# show as window pop up with TkAgg backend
+matplotlib.use('TkAgg')
+
 
 L = np.array([1, 2, 3])
 
@@ -23,8 +26,6 @@ L = np.random.random(100)
 data = pd.read_csv('../notebooks/data/president_heights.csv')
 heights = np.array(data['height(cm)'])
 
-# show as window pop up with TkAgg backend
-matplotlib.use('TkAgg')
 
 plt.hist(heights)
 plt.title('US Pres heights')
@@ -68,3 +69,29 @@ selection = X[indices]
 plt.scatter(X[:, 0], X[:, 1], alpha=0.3)
 plt.scatter(selection[:, 0], selection[:, 1],  s=200)
 plt.show()
+
+x = np.arange(10)
+i = np.array([2, 1, 8, 4])
+x[i] = 99
+
+x = np.zeros(10)
+np.add.at(x, [1, 2, 3], 1)
+
+np.random.seed(42)
+x = np.random.randn(100)
+
+bins = np.linspace(-5, 5, 20)
+
+counts = np.zeros_like(bins)
+
+i = np.searchsorted(bins, x)
+np.add.at(counts, i, 1)
+# XXX plt deprecates passing drawstyle
+plt.plot(bins, counts, linestyle='steps')
+plt.show()
+
+x = rand.rand(10, 2)
+plt.scatter(x[:, 0], x[:, 1], s=100)
+plt.show()
+
+dist_sq = np.sum((x[:, np.newaxis, :] - x[np.newaxis, :, :]) ** 2, axis=-1)
